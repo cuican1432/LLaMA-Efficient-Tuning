6c73c50778361825b56aedbfab16db138bb743f3
cuican1432 <cc5048@nyu.edu>
2023-07-17 22:32:21 +0800

    add 7b installation for test
    add 7b installation for test

--- run_7b.sh
+++ run_7b.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python install -r requirements.txt;
+pip install -r requirements.txt;
 
 
 CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --stage sft \
-    --model_name_or_path cuican1432/baichuan13b_dataphant \
+    --model_name_or_path /baichuan-inc/Baichuan-7B \
     --do_train \
     --padding_side right\
     --dataset ab_qa \
     --finetuning_type lora \
-    --output_dir Baichuan-13B-Base_checkpoint \
+    --output_dir Baichuan-7B-Base_checkpoint \
     --overwrite_cache \
     --lora_target W_pack \
     --per_device_train_batch_size 4 \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --logging_steps 10 \
     --save_steps 10 \
     --learning_rate 5e-5 \
-    --num_train_epochs 0.5 \
+    --num_train_epochs 5 \
     --plot_loss \
     --fp16
 ;
